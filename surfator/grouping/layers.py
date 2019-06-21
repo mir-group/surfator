d19e26d7d6b0a964c0af1e20848628795441ae30
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2019-06-21 15:41:05 -0400

    Added layer assignment by k-means

--- surfator/grouping/layers.py
+++ surfator/grouping/layers.py
@@ -8,8 +8,49 @@ from scipy.sparse.csgraph import connected_components
 
 from sitator.util import PBCCalculator
 
+import surfator
+
 import logging
 logger = logging.getLogger(__name__)
+
+
+def agree_within_layers_kmeans(initial_layer_heights, surface_normal = np.array([0, 0, 1]), min_layer_dist = 1):
+    """Assign mobile atoms to agreement groups based on their layer in the material.
+
+    In a system with fixed layers, like a surface slab (especially one with a
+    fixed bottom layer that constrains up-and-down movement), this algorithm
+    assigns atoms to layers by using k-means clustering in their "z" coordinate
+    (their coordinate along the surface normal).
+
+    Args:
+        - n_layers (int): The number of layers to find (k for k-means).
+        - surface_normal (3-vector): The unit normal vector to the surface.
+        - min_layer_dist (float, distance): The minimum height difference between
+            two layers' centers.
+    """
+    assert math.isclose(np.linalg.norm(surface_normal), 1.), "Given `surface_normal` is not a unit vector."
+    from sklearn.cluster import KMeans
+
+    n_layers = len(initial_layer_heights)
+    initial_layer_heights = initial_layer_heights.reshape(-1, 1)
+
+    # preallocate
+    clust_num_to_new_clust_num = np.empty(shape = n_layers, dtype = np.int)
+
+    def func(atoms):
+        # We need to wrap coordinates so that with slanted surfaces we get the
+        # right heights along the normal
+        atoms.wrap()
+        surf_zs = np.dot(surface_normal, atoms.get_positions().T)
+        assert len(surf_zs) == len(atoms)
+        kmeans = KMeans(n_clusters = n_layers, init = initial_layer_heights, n_init = 1).fit(surf_zs.reshape(-1, 1))
+        heights = kmeans.cluster_centers_.reshape(-1)
+        order = np.argsort(heights) # cluster numbers
+        assert np.min(heights[order][1:] - heights[order][:-1]) >= min_layer_dist, "Two layers were not far enough apart. Layer heights: %s" % heights
+        clust_num_to_new_clust_num[order] = np.arange(n_layers)
+        return clust_num_to_new_clust_num[kmeans.labels_]
+
+    return func
 
 
 def agree_within_layers(layer_heights, surface_normal = np.array([0, 0, 1])):
@@ -52,7 +93,7 @@ def agree_within_layers(layer_heights, surface_normal = np.array([0, 0, 1])):
     return func
 
 
-def agree_within_layers_and_deposits(layerfunc, surface_layer_index = 4, cutoff = 3):
+def agree_within_layers_and_deposits(layerfunc, surface_layer_index = 4, cutoff = 3, min_deposit_size = 3):
     """Define agreement groups based layers but allow surface deposits to be independent.
 
     Args:
@@ -79,9 +120,12 @@ def agree_within_layers_and_deposits(layerfunc, surface_layer_index = 4, cutoff 
         conn_mat = conn_mat < cutoff
         n_groups, groups = connected_components(conn_mat, directed = False)
         logger.debug("Found %i adatoms in %i independent deposits" % (np.sum(adatom_mask), n_groups))
-
+        group_trans = np.bincount(groups)
+        deposits = group_trans >= min_deposit_size
+        group_trans[~deposits] = surfator.AGREE_GROUP_NONE
         curr_max_agreegrp = np.max(tags)
-        tags[adatom_mask] = curr_max_agreegrp + groups
+        group_trans[deposits] = np.arange(np.sum(deposits)) + curr_max_agreegrp
+        tags[adatom_mask] = group_trans[groups]
         return tags
 
     return func
